17fde9d7d55ba9d8b465f729f77861d26d40570c
Daniel Załęski <gaa848@gmail.com>
2021-06-17 21:37:20 +0200

    remove sqllite db file

--- DTOs/ProductDTO.cs
+++ DTOs/ProductDTO.cs
@@ -6,15 +6,9 @@ namespace PwiAPI.DTOs
     public class ProductDTO
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "URL is required.")]
         public string Name { get; set; }
         public int Price { get; set; }
-
-        [Required(ErrorMessage = "Description")]
         public string Description { get; set; }
-
-        [Required(ErrorMessage = "URL is required.")]
-        [Url(ErrorMessage = "This isn't valid URL.")]
         public string ImageURL { get; set; }
         public CategoryDTO Category { get; set; }
     }
